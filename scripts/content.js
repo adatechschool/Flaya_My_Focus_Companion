a2f97e0d08439a5b34e89fb4c059dd914fc422db
Emilie VINCENT-SULLY <emivsy@gmail.com>
2025-04-14 14:10:07 +0200

    first step ok

--- scripts/content.js
+++ scripts/content.js
@@ -1,8 +1,17 @@
-const titles = document.querySelector('h1')
+const titles = document.querySelector('h1');
+const images = document.querySelector('img');
+const font = document.querySelector('p')
 
 const switchColors = () => {
-if (titles) {
-    titles.style.color = "green"
-}
-}
-switchColors()
+    if (titles) {
+        titles.style.color = "green"
+    }
+    if (images) {
+        images.style.backgroundColor = "red";
+    }
+
+    if(font) {
+        font.style.fontSize = '200px'
+    }
+    }
+    switchColors()
