1b6e2ca1e6330fe481141d57a6761356429a3aa6
Emilie VINCENT-SULLY <emivsy@gmail.com>
2025-04-17 15:13:52 +0200

    timer done

--- timer.js
+++ timer.js
@@ -1,26 +1,29 @@
 const testTimer = document.querySelector('#timer')
 
+ 
 function timer(userTime) {
     let minutes = userTime;
-    let secondes = minutes * 60;
+    let secondes =  60;
 
+    testTimer.innerText = `Il reste ${minutes} minutes`
+    
     const timing = setInterval(() => {
-        if (secondes % 60 == 0) {
+    if (secondes == 0) {
             minutes--
+            secondes = 60
         }
-        
+
         secondes-- 
 
-        if (secondes < 0) {
+        if (minutes === 0 && secondes === 0) {
             clearInterval(timing);
         }
-        testTimer.innerText = `Il reste ${minutes} et ${secondes}`
+        testTimer.innerText = `Il reste ${minutes-1} minutes et ${secondes} secondes`
 
     }, 1000)
 
 }
 
-// toutes les 60 sec il décrémente 1 min
 
 
 /* le user rentre 20 min
@@ -30,4 +33,4 @@ Dans 1 min, il y a  60 sec. Toutes les 60 sec on enlève 1 min
 à la fin du temps, on clear :  console log ("time out")
 
 */
-timer(2)
+timer(3)
